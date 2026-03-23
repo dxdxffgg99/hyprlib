a5d203cd28665bf2f96f3b67f40a11ef372190f3
dxdxffgg99 <dxdxffgg99@gmail.com>
2026-03-23 18:48:59 +0900

    i forgot undef

--- src/platform/cstdver.h
+++ src/platform/cstdver.h
@@ -12,21 +12,27 @@
     #define HYPR_C89 1
     #define HYPR_CSTD_NAME "C89"
 #elif __STDC_VERSION__ >= 202311L
+    #undef HYPR_C23
     #define HYPR_C23 1
     #define HYPR_CSTD_NAME "C23"
 #elif __STDC_VERSION__ >= 201710L
+    #undef HYPR_C17
     #define HYPR_C17 1
     #define HYPR_CSTD_NAME "C17"
 #elif __STDC_VERSION__ >= 201112L
+    #undef HYPR_C11
     #define HYPR_C11 1
     #define HYPR_CSTD_NAME "C11"
 #elif __STDC_VERSION__ >= 199901L
+    #undef HYPR_C99
     #define HYPR_C99 1
     #define HYPR_CSTD_NAME "C99"
 #elif __STDC_VERSION__ >= 199409L
+    #undef HYPR_C95
     #define HYPR_C95 1
     #define HYPR_CSTD_NAME "C95"
 #else
+    #undef HYPR_C99
     #define HYPR_C89 1
     #define HYPR_CSTD_NAME "C89"
 #endif
